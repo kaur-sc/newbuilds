e24ca1673f93984c98e348c9b35b69b5792ecf65
Kaur Martin <kaur@estateespana.com>
2025-12-19 17:01:58 +0100

    staging CSS fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,23 @@
 import { defineConfig } from 'vite'
-// Force restart
 import react from '@vitejs/plugin-react'
 import path from "path"
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ command }) => {
+  return {
+    // If we are building for production, use the subfolder. 
+    // Otherwise (local dev), use the root '/'
+    base: command === 'build' ? '/staging/newbuilds/' : '/',
+    
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  // @ts-expect-error - ssg options might not be strictly typed in default config
-  ssgOptions: {
-    script: 'async',
-    formatting: 'minify',
-  },
+    // @ts-expect-error - ssg options might not be strictly typed in default config
+    ssgOptions: {
+      script: 'async',
+      formatting: 'minify',
+    },
+  }
 })
